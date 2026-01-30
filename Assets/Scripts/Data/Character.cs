ecb60e0790cc6b8bd9d4f74c2f26ea828a304cb8
Jonathan S <scoffinjonathan@gmail.com>
2026-01-30 21:09:45 +0000

    Tobey work

--- Assets/Scripts/Data/Character.cs
+++ Assets/Scripts/Data/Character.cs
@@ -4,6 +4,22 @@ public enum Name
 {
     None,
     Kori,
+    Wiktor,
+    Toby,
+    Jamie,
+    Jonathan,
+    Safiya,
+    Max,
+    Lewis,
+    Abby,
+    Alex,
+    Katy,
+    Phoebe,
+    Jack,
+    Ethan,
+    Lenna,
+    Hannah,
+
 }
 
 public enum Mask
@@ -18,12 +34,22 @@ public enum Mask
 
 public enum Colour
 {
-
+    None,
+    Red,
+    Blue,
+    Green,
+    Yellow,
+    Orange,
 }
 
 public enum Activity
 {
-
+    None,
+    Drinking,
+    Walking,
+    Smoking,
+    Dancing,
+    Talking,
 }
 
 public class Character
@@ -40,28 +66,50 @@ public class Character
     public Activity activity;
     public Activity guessedActivity;
 
-    public static string GetMaskDisplayName(Mask mask)
+    public static string GetNameDisplayName(Name name)
     {
-        switch (mask)
+        switch (name)
         {
-            case Mask.None:
+            case Name.None:
                 return "???";
-            case Mask.Bunny:
-                return "Bunny";
-            case Mask.Dog:
-                return "Dog";
-            case Mask.Cat:
-                return "Cat";
-            case Mask.Rat:
-                return "Rat";
-            case Mask.Bird:
-                return "Bird";
+            case Name.Kori:
+                return "Kori";
+            case Name.Wiktor:
+                return "Wiktor";
+            case Name.Toby:
+                return "Toby";
+            case Name.Jamie:
+                return "Jamie";
+            case Name.Jonathan:
+                return "Jonathan";
+            case Name.Safiya:
+                return "Safiya";
+            case Name.Max:
+                return "Max";
+            case Name.Lewis:
+                return "Lewis";
+            case Name.Abby:
+                return "Abby";
+            case Name.Alex:
+                return "Alex";
+            case Name.Katy:
+                return "Katy";
+            case Name.Phoebe:
+                return "Phoebe";
+            case Name.Jack:
+                return "Jack";
+            case Name.Ethan:
+                return "Ethan";
+            case Name.Lenna:
+                return "Lenna";
+            case Name.Hannah:
+                return "Hannah";
             default:
                 return "???";
         }
     }
 
-    public static string GetColourDisplayName(Mask mask)
+    public static string GetMaskDisplayName(Mask mask)
     {
         switch (mask)
         {
@@ -84,41 +132,41 @@ public class Character
 
     public static string GetColourDisplayName(Colour colour)
     {
-        switch (mask)
+        switch (colour)
         {
-            case Mask.None:
+            case Colour.None:
                 return "???";
-            case Mask.Bunny:
-                return "Bunny";
-            case Mask.Dog:
-                return "Dog";
-            case Mask.Cat:
-                return "Cat";
-            case Mask.Rat:
-                return "Rat";
-            case Mask.Bird:
-                return "Bird";
+            case Colour.Red:
+                return "Red";
+            case Colour.Blue:
+                return "Blue";
+            case Colour.Green:
+                return "Green";
+            case Colour.Yellow:
+                return "Yellow";
+            case Colour.Orange:
+                return "Orange";
             default:
                 return "???";
         }
     }
 
-    public static string GetMaskDisplayName(Mask mask)
+    public static string GetActivityDisplayName(Activity activity)
     {
-        switch (mask)
+        switch (activity)
         {
-            case Mask.None:
+            case Activity.None:
                 return "???";
-            case Mask.Bunny:
-                return "Bunny";
-            case Mask.Dog:
-                return "Dog";
-            case Mask.Cat:
-                return "Cat";
-            case Mask.Rat:
-                return "Rat";
-            case Mask.Bird:
-                return "Bird";
+            case Activity.Drinking:
+                return "Drinking";
+            case Activity.Walking:
+                return "Walking";
+            case Activity.Smoking:
+                return "Smoking";
+            case Activity.Dancing:
+                return "Dancing";
+            case Activity.Talking:
+                return "Talking";
             default:
                 return "???";
         }
